2427d773729d2e7fb202bf90a95a5e15befcf5d9
HenryCROSS <henry583050973@gmail.com>
2023-08-24 19:40:37 -0400

    mv pin to own file

---
+++ vsc_sumoCode/include/pin.h
@@ -0,0 +1,17 @@
+#ifndef _PIN_H_
+
+#define LEFT_MOTOR 7            // Motor A enable pin
+#define RIGHT_MOTOR 12          // Motor B enable pin
+#define RIGHT_WHEEL_FORWARD 11  // Motor B In1 pin
+#define RIGHT_WHEEL_BACKWARD 10 // Motor B In2 pin
+#define LEFT_WHEEL_FORWARD 8    // Motor A In1 pin
+#define LEFT_WHEEL_BACKWARD 9   // Motor A In2 pin
+#define TRIGGER_PIN_L 2         // Ultrasonic sensor trigger pin
+#define ECHO_PIN_L 3            // Ultrasonic sensor echo pin
+#define TRIGGER_PIN_R 4         // Ultrasonic sensor trigger pin
+#define ECHO_PIN_R 5            // Ultrasonic sensor echo pin
+#define QTR_SENSOR_FL A1        // qtr sensor
+#define QTR_SENSOR_FR A0        // qtr sensor
+#define QTR_SENSOR_B A2         // qtr sensor
+
+#endif // !_PIN_H_
