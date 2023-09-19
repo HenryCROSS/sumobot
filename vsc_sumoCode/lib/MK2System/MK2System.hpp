5c1a0f75e92f9ca6d36ce2fcf1e910cb60b9f864
HenryCROSS <henry583050973@gmail.com>
2023-09-18 21:45:58 -0400

    update system

--- vsc_sumoCode/lib/MK2System/MK2System.hpp
+++ vsc_sumoCode/lib/MK2System/MK2System.hpp
@@ -5,7 +5,7 @@
 #include <Tools.hpp>
 #include <Vehicle_types.hpp>
 
-using Task = void (*)(MK2System::VehState);
+using Task = void (*)(MK2System::VehState&);
 
 class MK2System
 {
@@ -20,12 +20,15 @@ public:
     struct VehState
     {
         Stage stage;
+        int speed;
+        int range;
         Obj_direction ultra_info;
+        uint32_t custom_flag;
     };
 
     static void run();
-
     static bool register_task(const Task& task);
+
 private:
     static Array<Task, MAX_TASKS> tasks;
     static VehState state;
