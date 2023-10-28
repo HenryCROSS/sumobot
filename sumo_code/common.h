00c47a80b29fa3d97926651b3d0866fa4e8864c2
HenryCROSS <henry583050973@gmail.com>
2023-10-28 12:05:01 -0400

    reorg. the files

--- sumo_code/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_H_
-#define COMMON_H_
-
-#include <Arduino.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-#endif // COMMON_H_
--- sumo_code/type.h
+++
@@ -1,6 +0,0 @@
-#ifndef TYPE_H_
-#define TYPE_H_
-
-
-
-#endif // TYPE_H_
