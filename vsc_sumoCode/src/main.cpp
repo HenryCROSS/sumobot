01a48270ebefff3a9e290d1511a6a88266fa5983
HenryCROSS <henry583050973@gmail.com>
2023-11-11 01:07:55 -0500

    test oled

--- vsc_sumoCode/src/main.cpp
+++ vsc_sumoCode/src/main.cpp
@@ -209,26 +209,43 @@ void setup()
     MK2System::register_task(task_qtr, Task_Type::PREEMPTIVE, -1);
     MK2System::register_task(task_normal_attack, Task_Type::PREEMPTIVE, -1);
 
-    if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    { // Address 0x3D for 128x64
-        MK2System::register_task(task_oled_display, Task_Type::PREEMPTIVE, -1);
-        delay(2000);
-
-        g_state.monitor_running = true;
-
-        display.clearDisplay();
-        display.setTextSize(2);
-        display.setTextColor(WHITE);
-        display.setCursor(0, 10);
-        // Display static text
-        display.println("CHARGE!!");
-        display.display();
-    }
-    else
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
     {
         Serial.println(F("SSD1306 allocation failed"));
-        delay(2000);
+        for (;;)
+            ;
     }
+
+    delay(2000);
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    // Display static text
+    display.println("Hello, world!");
+    display.display();
+
+    // MK2System::register_task(task_oled_display, Task_Type::PREEMPTIVE, -1);
+    // if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    // { // Address 0x3D for 128x64
+    //     delay(2000);
+
+    //     g_state.monitor_running = true;
+
+    //     display.clearDisplay();
+    //     display.setTextSize(2);
+    //     display.setTextColor(WHITE);
+    //     display.setCursor(0, 10);
+    //     // Display static text
+    //     display.println("CHARGE!!");
+    //     display.display();
+    // }
+    // else
+    // {
+    //     Serial.println(F("SSD1306 allocation failed"));
+    //     delay(2000);
+    // }
 
     delay(4000);
 }
