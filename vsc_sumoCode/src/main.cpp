155e34430463e0916b970ca268b786a4c518d66b
HenryCROSS <henry583050973@gmail.com>
2023-12-31 01:06:41 -0500

    test

--- vsc_sumoCode/src/main.cpp
+++ vsc_sumoCode/src/main.cpp
@@ -422,7 +422,7 @@
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
